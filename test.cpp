c1926927d513fd3324d29f97ac963be8fac79206
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-03-02 16:19:44 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,14 +1,24 @@
 #include <Windows.h>
+
 #include <TlHelp32.h>
+
 #include <Psapi.h>
+
 #include <iostream>
+
 #include <string>
+
 #include <set>
+#include <string>
 #include <fstream>
+#include <cstring>
 #include <Dbghelp.h>
-
+#include <tuple>
+#include <cstdint>
+#include <winternl.h>
 #pragma comment(lib, "Dbghelp.lib")
-
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
+int (*hook)();
 // Suspended process threshold in milliseconds
 const DWORD SUSPENDED_PROCESS_THRESHOLD = 5000;
 
@@ -22,217 +32,284 @@ const DWORD RX_FLAGS = PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE
 const DWORD WCX_FLAGS = PAGE_WRITECOPY | PAGE_READWRITE | PAGE_WRITECOMBINE;
 
 // Suspicious API calls to monitor
-const char* SUSPICIOUS_APIS[] = {
-    "CreateRemoteThread",
-    "VirtualAllocEx",
-    "WriteProcessMemory",
-    "NtAllocateVirtualMemory",
-    "NtWriteVirtualMemory",
-    "NtCreateThreadEx",
-    "NtQueueApcThread",
-    "NtProtectVirtualMemory",
-    "NtMapViewOfSection",
-    "NtUnmapViewOfSection",
-    "NtCreateSection",
-    "NtCreateProcess",
-    "NtResumeThread",
-    "NtSuspendThread"
+const char * SUSPICIOUS_APIS[] = {
+  "CreateRemoteThread",
+  "VirtualAllocEx",
+  "WriteProcessMemory",
+  "NtAllocateVirtualMemory",
+  "NtWriteVirtualMemory",
+  "NtCreateThreadEx",
+  "NtQueueApcThread",
+  "NtProtectVirtualMemory",
+  "NtMapViewOfSection",
+  "NtUnmapViewOfSection",
+  "NtCreateSection",
+  "NtCreateProcess",
+  "NtResumeThread",
+  "NtSuspendThread"
 };
-const int NUM_SUSPICIOUS_APIS = sizeof(SUSPICIOUS_APIS) / sizeof(const char*);
+const int NUM_SUSPICIOUS_APIS = sizeof(SUSPICIOUS_APIS) / sizeof(const char * );
 
 // Set to store PIDs with suspicious indicators
-std::set<DWORD> suspicious_pids;
+std::set < DWORD > suspicious_pids;
 
 // Set to store already dumped PIDs
-std::set<DWORD> dumped_pids;
+std::set < DWORD > dumped_pids;
 
 // Helper function to dump process memory
-// Helper function to dump process memory
-void dump_process_memory(HANDLE process, const std::string& dump_file_path) {
-    // Open file for writing
-    std::ofstream dump_file(dump_file_path, std::ios::binary);
-    // Get process memory information
-    SYSTEM_INFO system_info;
-    GetSystemInfo(&system_info);
-    MEMORY_BASIC_INFORMATION memory_info;
+void dump_process_memory(HANDLE process,
+  const std::string & dump_file_path) {
+  // Open file for writing
+  std::ofstream dump_file(dump_file_path, std::ios::binary);
+  // Get process memory information
+  SYSTEM_INFO system_info;
+  GetSystemInfo( & system_info);
+  MEMORY_BASIC_INFORMATION memory_info;
 
-    // Dump memory for each region of the process
-    for (LPVOID address = system_info.lpMinimumApplicationAddress; address < system_info.lpMaximumApplicationAddress; address = memory_info.BaseAddress + memory_info.RegionSize) {
-        // Get memory information for the region
-        VirtualQueryEx(process, address, &memory_info, sizeof(memory_info));
+  // Dump memory for each region of the process
+  for (LPVOID address = system_info.lpMinimumApplicationAddress; address < system_info.lpMaximumApplicationAddress; address = memory_info.BaseAddress + memory_info.RegionSize) {
+    // Get memory information for the region
+    VirtualQueryEx(process, address, & memory_info, sizeof(memory_info));
 
-        // Skip regions with no access
-        if (memory_info.Protect == PAGE_NOACCESS) {
-            continue;
-        }
-
-        // Skip regions that have already been dumped
-        if (dumped_pids.count(reinterpret_cast<uintptr_t>(memory_info.BaseAddress)) > 0) {
-            continue;
-        }
-
-        // Calculate chunk size for dumping
-        SIZE_T chunk_size = memory_info.RegionSize;
-        if (memory_info.Protect == PAGE_GUARD) {
-            chunk_size = system_info.dwPageSize;
-        }
-
-        // Allocate buffer for dumping memory
-        LPVOID buffer = VirtualAlloc(nullptr, chunk_size, MEM_COMMIT, PAGE_READWRITE);
-        if (!buffer) {
-            continue;
-        }
-
-        // Dump memory to file
-        SIZE_T bytes_written;
-        uintptr_t total_bytes_written = reinterpret_cast<uintptr_t>(memory_info.BaseAddress);
-        while (total_bytes_written < reinterpret_cast<uintptr_t>(memory_info.BaseAddress) + memory_info.RegionSize) {
-            if (!ReadProcessMemory(process, reinterpret_cast<LPCVOID>(total_bytes_written), buffer, chunk_size, &bytes_written)) {
-                break;
-            }
-            dump_file.write(reinterpret_cast<const char*>(buffer), bytes_written);
-            total_bytes_written += bytes_written;
-        }
-
-        // Free buffer
-        VirtualFree(buffer, 0, MEM_RELEASE);
-
-        // Add dumped region to set
-        dumped_pids.insert(reinterpret_cast<uintptr_t>(memory_info.BaseAddress));
+    // Skip regions with no access
+    if (memory_info.Protect == PAGE_NOACCESS) {
+      continue;
     }
 
+    // Skip regions that have already been dumped
+    if (dumped_pids.count(reinterpret_cast < uintptr_t > (memory_info.BaseAddress)) > 0) {
+      continue;
+    }
+
+    // Calculate chunk size for dumping
+    SIZE_T chunk_size = memory_info.RegionSize;
+    if (memory_info.Protect == PAGE_GUARD) {
+      chunk_size = system_info.dwPageSize;
+    }
+
+    // Allocate buffer for dumping memory
+    LPVOID buffer = VirtualAlloc(nullptr, chunk_size, MEM_COMMIT, PAGE_READWRITE);
+    if (!buffer) {
+      continue;
+    }
+
+    // Dump memory to file
+    SIZE_T bytes_written;
+    uintptr_t total_bytes_written = reinterpret_cast < uintptr_t > (memory_info.BaseAddress);
+    while (total_bytes_written < reinterpret_cast < uintptr_t > (memory_info.BaseAddress) + memory_info.RegionSize) {
+      if (!ReadProcessMemory(process, reinterpret_cast < LPCVOID > (total_bytes_written), buffer, chunk_size, & bytes_written)) {
+        break;
+      }
+      dump_file.write(reinterpret_cast <
+        const char * > (buffer), bytes_written);
+      total_bytes_written += bytes_written;
+    }
     // Close file
     dump_file.close();
+  }
 }
 
-
-// Suspicious API hook function
-DWORD WINAPI suspicious_api_hook(LPVOID lpParam) {
-// Get API parameters
-const char* api_name = reinterpret_cast<const char*>(lpParam);
-const HANDLE process = GetCurrentProcess();
-// Dump process memory
-char dump_file_name[MAX_PATH];
-GetTempPathA(MAX_PATH, dump_file_name);
-strcat_s(dump_file_name, MAX_PATH, api_name);
-strcat_s(dump_file_name, MAX_PATH, "_dump.bin");
-dump_process_memory(process, dump_file_name);
-
-// Add PID to set of suspicious PIDs
-DWORD pid = GetProcessId(process);
-suspicious_pids.insert(pid);
-
-// Return control to original API function
-return 0;
+// Helper function to check if a process is suspended
+bool is_process_suspended(DWORD pid) {
+  HANDLE process = OpenProcess(PROCESS_SUSPEND_RESUME, FALSE, pid);
+  if (!process) {
+    return false;
+  }
+  bool result = (WaitForSingleObject(process, SUSPENDED_PROCESS_THRESHOLD) == WAIT_TIMEOUT);
+  CloseHandle(process);
+  return result;
 }
 
-// Hook API function with suspicious_api_hook
-void hook_api(const char* api_name) {
-HMODULE module = GetModuleHandle(nullptr);
-FARPROC original_func = GetProcAddress(module, api_name);
-// Create trampoline function
-BYTE trampoline_bytes[] = {
-    0x48, 0xb8 // mov rax, <suspicious_api_hook>
-};
-*reinterpret_cast<void**>(&trampoline_bytes[2]) = reinterpret_cast<void*>(&suspicious_api_hook);
-trampoline_bytes[10] = 0xff; // jmp rax
-
-// Allocate executable memory for trampoline
-LPVOID trampoline_memory = VirtualAlloc(nullptr, sizeof(trampoline_bytes), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-memcpy(trampoline_memory, trampoline_bytes, sizeof(trampoline_bytes));
-
-// Patch original function with trampoline
-DWORD old_protection;
-VirtualProtect(reinterpret_cast<LPVOID>(original_func), sizeof(trampoline_bytes), PAGE_EXECUTE_READWRITE, &old_protection);
-memcpy(reinterpret_cast<void*>(original_func), trampoline_bytes, sizeof(trampoline_bytes));
-VirtualProtect(reinterpret_cast<LPVOID>(original_func), sizeof(trampoline_bytes), PAGE_EXECUTE_READWRITE, &old_protection);
-
-}
-
-// Monitor process for suspicious activity
-void monitor_process(DWORD pid) {
-// Open process handle
-HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-if (!process) {
-return;
-}
-// Check if process is already in set of suspicious PIDs
-if (suspicious_pids.count(pid) > 0) {
-    return;
-}
-
-// Check if process is already in set of dumped PIDs
-if (dumped_pids.count(pid) > 0) {
-    return;
-}
-
-// Check if process is suspended
-DWORD process_suspend_count = SuspendThread(process);
-if (process_suspend_count == (DWORD)-1) {
+// Helper function to check if a process is running from a temporary directory
+bool is_process_running_from_temp_dir(DWORD pid) {
+  HANDLE process = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
+  if (!process) {
+    return false;
+  }
+  // Get the process image file name
+  char image_file_name[MAX_PATH];
+  if (GetProcessImageFileNameA(process, image_file_name, MAX_PATH) == 0) {
     CloseHandle(process);
-    return;
-}
-if (process_suspend_count > 0) {
-    Sleep(SUSPENDED_PROCESS_THRESHOLD);
-    DWORD process_resume_count = ResumeThread(process);
-    CloseHandle(process);
-    return;
+    return false;
+  }
+  // Get the path of the parent directory
+  std::string parent_dir = image_file_name;
+  parent_dir = parent_dir.substr(0, parent_dir.find_last_of("\/"));
+  // Check if the parent directory is a temp directory
+  char temp_dir[MAX_PATH];
+  GetTempPathA(MAX_PATH, temp_dir);
+  bool result = (parent_dir.find(temp_dir) != std::string::npos);
+
+  CloseHandle(process);
+  return result;
 }
 
-// Enumerate process modules
-HMODULE modules[1024];
-DWORD bytes_needed;
-if (EnumProcessModules(process, modules, sizeof(modules), &bytes_needed)) {
-    // Get number of modules
-    DWORD num_modules = bytes_needed / sizeof(HMODULE);
+// Helper function to check if a memory region is suspicious
+bool is_memory_region_suspicious(HANDLE process,
+  const MEMORY_BASIC_INFORMATION & memory_info) {
+  // Check for suspicious memory protection flags
+  DWORD protection = memory_info.Protect;
+  if ((protection & RWX_FLAGS) || (protection & RX_FLAGS) || (protection & WCX_FLAGS)) {
+    // Check if memory region is backed by a file
+    MEMORY_BASIC_INFORMATION memory_info_file;
+    VirtualQueryEx(process, memory_info.BaseAddress, & memory_info_file, sizeof(memory_info_file));
+    if (memory_info_file.Type == MEM_MAPPED) {
+      return false;
+    }
+    // Check if memory region has been dumped before
+    if (dumped_pids.count(reinterpret_cast < uintptr_t > (memory_info.BaseAddress)) > 0) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+std::tuple<HMODULE, FARPROC> split_dll_name_and_function_name(const char* name) {
+    auto colon_pos = std::strchr(name, ':');
+    if (colon_pos == nullptr) {
+        return { nullptr, nullptr };
+    }
+    std::string dll_name(name, colon_pos);
 
-    // Check each module for RWX or RX memory
-    for (DWORD i = 0; i < num_modules; i++) {
-        MODULEINFO module_info;
-        if (GetModuleInformation(process, modules[i], &module_info, sizeof(module_info))) {
-// Check if module has RWX or RX memory protection
-uintptr_t module_protection = reinterpret_cast<uintptr_t>(module_info.lpBaseOfDll);
-if ((module_protection & RWX_FLAGS) || (module_protection & RX_FLAGS)) {
-// Dump process memory and add PID to set of suspicious PIDs
-char dump_file_name[MAX_PATH];
-GetTempPathA(MAX_PATH, dump_file_name);
-strcat_s(dump_file_name, MAX_PATH, "module_dump.bin");
-dump_process_memory(process, dump_file_name);
-DWORD pid = GetProcessId(process);
-suspicious_pids.insert(pid);
-break;
+
+
+
+    std::string function_name(colon_pos + 1);
+    auto module = GetModuleHandleA(dll_name.c_str());
+    if (module == nullptr) {
+        module = LoadLibraryA(dll_name.c_str());
+        if (module == nullptr) {
+            return { nullptr, nullptr };
+        }
+    }
+    auto function = GetProcAddress(module, function_name.c_str());
+    return { module, function };
 }
-}
-}
-// Hook suspicious APIs
-for (int i = 0; i < NUM_SUSPICIOUS_APIS; i++) {
-    hook_api(SUSPICIOUS_APIS[i]);
+// Helper function to check if a process is injecting code
+bool is_process_injecting_code(HANDLE process) {
+    // Array of suspicious API names
+    const char* SUSPICIOUS_APIS[] = {
+        "kernel32.dll:LoadLibraryA",
+        "kernel32.dll:VirtualAllocEx",
+        "kernel32.dll:WriteProcessMemory",
+        "kernel32.dll:CreateRemoteThread",
+        "ntdll.dll:NtCreateThreadEx",
+        "kernel32.dll:RtlCreateUserThread",
+        "kernel32.dll:CreateThread",
+        "ntdll.dll:NtWriteVirtualMemory",
+        "ntdll.dll:NtAllocateVirtualMemory",
+        "ntdll.dll:NtCreateSection",
+        "ntdll.dll:NtMapViewOfSection",
+    };
+    constexpr size_t SUSPICIOUS_APIS_COUNT = sizeof(SUSPICIOUS_APIS) / sizeof(SUSPICIOUS_APIS[0]);
+
+    // Iterate through each suspicious API
+    for (size_t i = 0; i < SUSPICIOUS_APIS_COUNT; i++) {
+        HMODULE module;
+        FARPROC function;
+        std::tie(module, function) = split_dll_name_and_function_name(SUSPICIOUS_APIS[i]);
+
+        if (function == nullptr) {
+            continue;
+        }
+
+        // Get the address of the function in the process being scanned
+        auto functionPtr = reinterpret_cast<long long int(*)()>(GetProcAddress(module, function));
+
+        if (functionPtr == nullptr) {
+            continue;
+        }
+
+        // Install the hook for each function with this name in the process being scanned
+        for (size_t j = 0; functionPtr[j] != nullptr; j++) {
+            union FunctionPointer {
+                void* ptr;
+                void(*fn)();
+            };
+            FunctionPointer original_function = { .ptr = functionPtr[j] };
+            uint8_t trampoline_bytes[sizeof(FunctionPointer) + 5] = { 0 };
+            trampoline_bytes[0] = 0xE9; // JMP rel32
+            void* &trampoline_ptr = reinterpret_cast<void*&>(trampoline_bytes[1]);
+            trampoline_ptr = original_function.ptr;
+            void* &trampoline_offset_ptr = reinterpret_cast<void*&>(trampoline_bytes[5]);
+            trampoline_offset_ptr = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(&hook) - reinterpret_cast<uintptr_t>(&trampoline_bytes[1]) - 4);
+
+            DWORD old_protect;
+            if (!VirtualProtect(original_function.ptr, sizeof(trampoline_bytes), PAGE_EXECUTE_READWRITE, &old_protect)) {
+                return true;
+            }
+
+            uint8_t original_bytes[sizeof(trampoline_bytes)];
+            memcpy(original_bytes, original_function.ptr, sizeof(trampoline_bytes));
+
+            if (memcmp(original_bytes, trampoline_bytes, sizeof(trampoline_bytes)) == 0) {
+                continue;
+            }
+
+            if (!WriteProcessMemory(process, original_function.ptr, trampoline_bytes, sizeof(trampoline_bytes), nullptr)) {
+                return true;
+            }
+
+            if (!VirtualProtect(original_function.ptr, sizeof(trampoline_bytes), old_protect, &old_protect)) {
+                return true;
+            }
+
+            uint8_t jmp_bytes[5] = { 0 };
+            jmp_bytes[0] = 0xE9; // JMP rel32
+            int32_t& jmp_ptr = *reinterpret_cast<int32_t*>(&jmp_bytes[1]);
+            jmp_ptr = static_cast<int32_t>(reinterpret_cast<uintptr_t>(&hook) - reinterpret_cast<uintptr_t>(original_function.ptr) - 5);
+
+                    if (!VirtualProtect(original_function.ptr, sizeof(jmp_bytes), PAGE_EXECUTE_READWRITE, &old_protect)) {
+            return true;
+        }
+
+        uint8_t original_jmp_bytes[sizeof(jmp_bytes)];
+        memcpy(original_jmp_bytes, original_function.ptr, sizeof(jmp_bytes));
+
+        if (memcmp(original_jmp_bytes, jmp_bytes, sizeof(jmp_bytes)) == 0) {
+            continue;
+        }
+
+        if (!WriteProcessMemory(process, original_function.ptr, jmp_bytes, sizeof(jmp_bytes), nullptr)) {
+            return true;
+        }
+
+        if (!VirtualProtect(original_function.ptr, sizeof(jmp_bytes), old_protect, &old_protect)) {
+            return true;
+        }
+    }
 }
 
-// Resume process
-ResumeThread(process);
-
-// Close process handle
-CloseHandle(process);
-}
+return false;
 }
 
-int main() {
-// Get list of running processes
-DWORD processes[1024];
-DWORD bytes_needed;
-if (EnumProcesses(processes, sizeof(processes), &bytes_needed)) {
-// Get number of processes
-DWORD num_processes = bytes_needed / sizeof(DWORD);
-// Monitor each process
-for (DWORD i = 0; i < num_processes; i++) {
-    monitor_process(processes[i]);
-}
-}
 
-// Wait for user input before exiting
-std::cout << "Press enter to exit..." << std::endl;
-std::cin.get();
 
-return 0;
+int main(int argc, char * argv[]) {
+  // Get list of running processes
+  DWORD pids[1024];
+  DWORD needed;
+  if (!EnumProcesses(pids, sizeof(pids), & needed)) {
+    return 1;
+  }
+  int num_processes = needed / sizeof(DWORD);
+  // Iterate over processes and check for suspicious indicators
+  for (int i = 0; i < num_processes; i++) {
+    DWORD pid = pids[i];
+    if (pid != 0 && pid != 4) { // skip System Idle Process and System
+      HANDLE process = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
+      if (process) {
+        // Check for suspicious indicators
+        if (is_process_suspended(pid) || is_process_running_from_temp_dir(pid) || is_process_injecting_code(process)) {
+          // Dump process memory
+          char dump_file_path[MAX_PATH];
+          sprintf_s(dump_file_path, MAX_PATH, "process_%u.dmp", pid);
+          dump_process_memory(process, dump_file_path);
+          dumped_pids.insert(reinterpret_cast < uintptr_t > (process));
+        }
+        CloseHandle(process);
+      }
+    }
+  }
+  return 0;
 }
